d0490dea85ea383845adc7166a6bdbd39de3a61a
ryowuandjanet <ryowu0329@gmail.com>
2021-10-23 22:25:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter" />
+  </div>
+</template>
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
